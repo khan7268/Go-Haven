32b488114a18a6e92764ef0f2483f8d68015714e
Arman Khan <ak7268080602@gmail.com>
2025-05-05 16:55:35 +0530

    Update IndexPage.jsx

--- frontend/src/components/IndexPage.jsx
+++ frontend/src/components/IndexPage.jsx
@@ -19,7 +19,7 @@ function IndexPage() {
               {place.photos?.[0] && (
                 <img
                   className="rounded-2xl object-cover aspect-square"
-                  src={"http://localhost:4000/uploads/" + place.photos?.[0]}
+                  src={"https://airbnb-mern-sibb.onrender.com/uploads/" + place.photos?.[0]}
                   alt=""
                 />
               )}
