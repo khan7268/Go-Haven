b5fffb04ea13d52faa3d40d43ec575d83136cf77
Arman Khan <ak7268080602@gmail.com>
2025-05-05 23:52:30 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import UserContext from '../userContext'
+import UserContext from '../UserContext'
 
 function Header() {
   const { user } = useContext(UserContext);
