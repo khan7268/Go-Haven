a8e981e20c9b686a06085ee42ded8601e9eb1f7c
Arman Khan <ak7268080602@gmail.com>
2025-05-15 10:31:14 +0530

    Update PlacesPage.jsx

--- frontend/src/components/PlacesPage.jsx
+++ frontend/src/components/PlacesPage.jsx
@@ -195,6 +195,13 @@ const handleDeleteClose = () => {
   return (
     <div>
       <AccountNav />
+     <div className='text-center'>
+        <Link className='inline-flex gap-1 bg-[#F5385D] text-white px-6 py-2 rounded-full' to={'/account/places/new'}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>           <span className='ml-2'>List Your Stay</span>
+        </Link>
+      </div>
       <div className="mt-6 flex flex-col gap-6">
         {places.length > 0 &&
           places.map((place) => (
