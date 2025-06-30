b61f56531c79e56cbf6e0b66d59fbf34254974c5
Arman Khan <ak7268080602@gmail.com>
2025-06-30 23:15:54 +0530

    Update IndexPage.jsx

--- frontend/src/components/IndexPage.jsx
+++ frontend/src/components/IndexPage.jsx
@@ -59,7 +59,7 @@ function IndexPage() {
             <Link to={'/place/' + place._id}>
               <div className="bg-gray-500 mb-2 rounded-2xl flex">
                 {place.photos?.[0] && (
-                  <img
+                   <img
                     className="rounded-2xl object-cover aspect-square"
                     src={"https://go-haven.onrender.com/uploads/" + place.photos?.[0]}
                     alt=""
